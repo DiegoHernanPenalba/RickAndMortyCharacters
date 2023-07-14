d9aaf9eea03357dab893359643c24521b02a4d38
DiegoHernanPenalba <diego@muuvlabs.com>
2023-07-14 10:04:39 -0300

    fist workspace commit

---
+++ RickAndMortyCharacters/RickAndMortyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCharactersApp.swift
+//  RickAndMortyCharacters
+//
+//  Created by Diego Hernan Peñalba on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
